e6d3a2f2fe1b2191574876dce2688f1d259d751e
andrew <andrewtengzr@gmail.com>
2023-04-20 09:41:28 +0800

    section-4: self-practice

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -4,7 +4,7 @@
 // module.exports
 const C = require("./test-module-1");
 const calc1 = new C();
-console.log(calc1.add(2, 5));
+console.log(calc1.add(10, 5));
 
 // exports
 // const calc2 = require("./test-module-2");
--- 2-how-node-works/starter/test-module-1.js
+++ 2-how-node-works/starter/test-module-1.js
@@ -1,18 +1,5 @@
-// class Calculator {
-//     add(a, b) {
-//         return a + b;
-//     }
-
-//     multiply(a, b) {
-//         return a * b;
-//     }
-
-//     divide(a, b) {
-//         return a / b;
-//     }
-// };
-
-module.exports = class {
+// method 1: name and define a class before exporting it
+class Calculator {
     add(a, b) {
         return a + b;
     }
@@ -25,3 +12,20 @@ module.exports = class {
         return a / b;
     }
 };
+
+module.exports = Calculator
+
+// // method 2: directly exporting the defination without naming the class
+// module.exports = class {
+//     add(a, b) {
+//         return a + b;
+//     }
+
+//     multiply(a, b) {
+//         return a * b;
+//     }
+
+//     divide(a, b) {
+//         return a / b;
+//     }
+// };
