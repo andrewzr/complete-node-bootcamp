bb645b938f54c6caedad24657bb6abf25ffed78d
andrew <andrewtengzr@gmail.com>
2023-04-25 09:11:47 +0800

    section-5: added try/catch for error in promise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -33,9 +33,29 @@ const getDocPic = async () => {
             console.log("Random dog image saved to file!");
     } catch (err) {
         console.log(err);
+        throw err;
     }
+    return "2: DOGGO READY!"
 };
-getDocPic();
+
+(async () => {
+    try {
+        console.log("1: Will get dog pics!")
+        const x = await getDocPic();
+        console.log(x);
+        console.log("3: Done getting dog pics!")
+    } catch (err) {
+        console.log("Error in promise");
+    }
+})();
+
+// console.log("1: Will get dog pics!")
+// getDocPic().then(x  => {
+//     console.log(x);
+//     console.log("3: Done getting dog pics!")
+// }).catch(err => {
+//     console.log("Error in promise");
+// });
 
 // readFilePro(`${__dirname}/dog.txt`)
 //     .then(data => {
