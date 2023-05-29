b2959c4e6cec7600d48a06c23c8d30a5c3b3b1ea
andrew <andrewtengzr@gmail.com>
2023-05-29 09:14:03 +0800

    section 6: formatting json

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -31,7 +31,6 @@ app.post("/api/v1/tours", (req, res) => {
     console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
-
     tours.push(newTour);
 
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
